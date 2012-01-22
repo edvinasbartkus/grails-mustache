eacbcd8038258b3f7342f5c7b7dec32af6ceaf3c
Edvinas Bartkus <edvinas@geeks.lt>
2012-01-23 00:35:29 +0200

    Test for mustache:render

--- test/unit/org/grails/plugins/mustache/MustacheTagLibTests.groovy
+++ test/unit/org/grails/plugins/mustache/MustacheTagLibTests.groovy
@@ -1,17 +1,29 @@
 package org.grails.plugins.mustache
 
-
-
 import grails.test.mixin.*
 import org.junit.*
 
-/**
- * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
- */
 @TestFor(MustacheTagLib)
 class MustacheTagLibTests {
 
-    void testSomething() {
-        fail "Implement me"
-    }
+  void testRender() {
+    StringWriter out = new StringWriter()
+    MustacheTagLib.metaClass.out = out
+    
+    new MustacheTagLib().render([
+      model: [
+        name: "Edvinas",
+        value: 2,
+        taxed_value: 1.5,
+        in_ca: true
+      ]
+    ], { ->
+      return '''Hello {{name}}. You have just won ${{value}}! {{#in_ca}} Well, ${{taxed_value}}, after taxes. {{/in_ca}}'''
+    })
+
+    assertEquals 'Hello Edvinas. You have just won $2!  Well, $1.5, after taxes. ', out.toString()
+
+    def remove = GroovySystem.metaClassRegistry.&removeMetaClass
+    remove MustacheTagLib
+  }
 }
